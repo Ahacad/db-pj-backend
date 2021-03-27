94f57373142f59e8b9e6dc7a5e10fe3dcb4ed67c
ahacad <1393345075@qq.com>
2021-03-27 10:07:23 +0800

    feat: add https

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const https = require("https");
+const fs = require("fs");
 const bodyParser = require("body-parser");
 const app = express();
 const port = 4000;
 
-const db = require("./queries");
+const options = {
+  key: fs.readFileSync(__dirname + "/localhost-key.pem"),
+  cert: fs.readFileSync(__dirname + "/localhost.pem"),
+};
 
 app.use(bodyParser.json());
 app.use(
@@ -20,9 +25,15 @@ app.get("/", (req, resp) => {
   resp.json({ info: "Node.js, api" });
 });
 
+const db = require("./queries");
 app.get("/users", db.getUsers);
 app.get("/users/:id", db.getUserById);
 
-app.listen(port, () => {
-  console.log(`app running on port ${port}`);
+//app.listen(port, () => {
+//console.log(`app running on port ${port}`);
+//});
+
+const server = https.createServer(options, app);
+server.listen(port, () => {
+  console.log(`server running on port ${port}`);
 });
