6b59ba2feb86f90c905b6d01536ceb2636f89f85
ahacad <1393345075@qq.com>
2021-04-01 22:28:57 +0800

    fix: api throw error problem

--- api/index.js
+++ api/index.js
@@ -1,12 +1,3 @@
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  user: 'ahacad',
-  password: 'root',
-  host: 'localhost',
-  database: 'api',
-  port: 5432,
-});
 const { authClient } = require('./users');
 
 module.exports = {
--- api/users.js
+++ api/users.js
@@ -1,7 +1,17 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'ahacad',
+  password: 'root',
+  host: 'localhost',
+  database: 'api',
+  port: 5432,
+});
+
 const getUsers = (req, resp) => {
   pool.query('SELECT * FROM user ORDER BY id ASC', (err, res) => {
     if (err) {
-      throw err;
+      console.error(err);
     }
     resp.status(200).json(res.rows);
   });
@@ -12,7 +22,7 @@ const getUserById = (req, resp) => {
 
   pool.query('SELECT * FROM user WHERE id = $1', [id], (err, res) => {
     if (err) {
-      throw err;
+      console.error(err);
     }
     resp.status(200).json(res.rows);
   });
@@ -23,7 +33,7 @@ const getUserByName = (req, resp) => {
 
   pool.query('SELECT * FROM user WHERE name = $1', [name], (err, res) => {
     if (err) {
-      throw err;
+      console.error(err);
     }
     resp.status(200).json(res.rows);
   });
@@ -33,7 +43,7 @@ const getUserByEmail = (req, resp) => {
   const { email } = req.params;
   pool.query('SELECT * FROM user WHERE email = $1', [email], (err, res) => {
     if (err) {
-      throw err;
+      console.error(err);
     }
     resp.status(200).json(res.rows);
   });
@@ -49,9 +59,9 @@ const createUser = (req, resp) => {
     [name, email, password, createTime],
     (err, res) => {
       if (err) {
-        throw err;
+        console.error(err);
       }
-      resp.status(201).send(`user inserted, id: ${res.insertId}`);
+      resp.status(201).send('user inserted');
     },
   );
 };
@@ -61,7 +71,7 @@ const updateUser = (req, resp) => {
   const { name, bio } = req.body;
   pool.query('UPDATE user SET name = $1, bio = $2', [name, bio], (err, res) => {
     if (err) {
-      throw err;
+      console.error(err);
     }
     resp.status(200).send(`user ${id} info updated`);
   });
@@ -72,7 +82,7 @@ const deleteUser = (req, resp) => {
 
   pool.query('DELETE FROM user WHERE id = $1', [id], (err, res) => {
     if (err) {
-      throw err;
+      console.error(err);
     }
     resp.status(200).send(`User deleted, id: ${id}`);
   });
