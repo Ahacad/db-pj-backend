8dc557acc75dc7c3907d29aa15944fccf8c8ced6
ahacad <1393345075@qq.com>
2021-04-01 20:45:27 +0800

    feat: add eslint

--- api/index.js
+++ api/index.js
@@ -1,12 +1,13 @@
-const { Pool } = require("pg");
+const { Pool } = require('pg');
+
 const pool = new Pool({
-  user: "ahacad",
-  password: "root",
-  host: "localhost",
-  database: "api",
+  user: 'ahacad',
+  password: 'root',
+  host: 'localhost',
+  database: 'api',
   port: 5432,
 });
-const { authClient } = require("./users");
+const { authClient } = require('./users');
 
 module.exports = {
   authClient,
