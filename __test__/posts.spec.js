0d6f1fd1e7c61abbb6e9758f7e93ffce75d8470c
ahacad <1393345075@qq.com>
2021-04-02 17:32:35 +0800

    feat(tests): add tests for post

---
+++ __test__/posts.spec.js
@@ -0,0 +1,9 @@
+const supertest = require('supertest');
+const app = require('../app');
+
+describe('posts API', () => {
+  it('add post', async () => {
+    // TODO
+    expect(2).toBe(2);
+  });
+});
