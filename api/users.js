1351943a0b64950a556b6a33a33b6bd889619fff
ahacad <1393345075@qq.com>
2021-04-04 15:54:50 +0800

    feat(api/users): rewrite all apis with promises

--- api/users.js
+++ api/users.js
@@ -21,34 +21,43 @@ const getUsers = (req, resp) => {
 
 const getUserById = (req, resp) => {
   const id = parseInt(req.params.id, 10);
-
-  pool.query('SELECT * FROM user WHERE id = $1', [id], (err, res) => {
-    if (err) {
+  pool
+    .connect()
+    .then((client) => client.query('SELECT * FROM user WHERE id = $1', [id]).then((res) => {
+      client.release();
+      resp.status(200).json(res.rows);
+    }))
+    .catch((err) => {
       console.error(err);
-    }
-    resp.status(200).json(res.rows);
-  });
+    });
 };
 
 const getUserByName = (req, resp) => {
   const { name } = req.params;
-
-  pool.query('SELECT * FROM user WHERE name = $1', [name], (err, res) => {
-    if (err) {
+  pool
+    .connect()
+    .then((client) => client.query('SELECT * FROM user WHERE name = $1', [name]).then((res) => {
+      client.release();
+      resp.status(200).json(res.rows);
+    }))
+    .catch((err) => {
       console.error(err);
-    }
-    resp.status(200).json(res.rows);
-  });
+    });
 };
 
 const getUserByEmail = (req, resp) => {
   const { email } = req.params;
-  pool.query('SELECT * FROM user WHERE email = $1', [email], (err, res) => {
-    if (err) {
+  pool
+    .connect()
+    .then((client) => client
+      .query('SELECT * FROM user WHERE email = $1', [email])
+      .then((res) => {
+        client.release();
+        resp.status(200).json(res.rows);
+      }))
+    .catch((err) => {
       console.error(err);
-    }
-    resp.status(200).json(res.rows);
-  });
+    });
 };
 
 const login = (req, resp) => {
@@ -56,20 +65,25 @@ const login = (req, resp) => {
   const saltedPassword = crypto
     .pbkdf2Sync(password, salt, 1000, 64, 'sha512')
     .toString('hex');
-  pool.query('SELECT * FROM users WHERE email = $1', [email], (err, res) => {
-    if (err) {
+  pool
+    .connect()
+    .then((client) => client
+      .query('SELECT * FROM users WHERE email = $1', [email])
+      .then((res) => {
+        client.release();
+        if (res.rows[0] === undefined) {
+          resp.status(404).send('user not found');
+          // TODO: test manually
+        } else if (res.rows[0].password !== saltedPassword) {
+          console.log(res.rows[0]);
+          resp.status(403).send('password wrong');
+        } else {
+          resp.status(200).send(res.rows[0]);
+        }
+      }))
+    .catch((err) => {
       console.error(err);
-    }
-    if (res.rows[0] === undefined) {
-      resp.status(404).send('user not found');
-      // TODO: test manually
-    } else if (res.rows[0].password !== saltedPassword) {
-      console.log(res.rows[0]);
-      resp.status(403).send('password wrong');
-    } else {
-      resp.status(200).send(res.rows[0]);
-    }
-  });
+    });
 };
 
 const createUser = (req, resp) => {
@@ -79,16 +93,20 @@ const createUser = (req, resp) => {
     .pbkdf2Sync(password, salt, 1000, 64, 'sha512')
     .toString('hex');
   // TODO: one email only
-  pool.query(
-    'INSERT INTO users (name, email, password, create_time) VALUES ($1, $2, $3, $4) RETURNING id;',
-    [name, email, saltedPassword, createTime],
-    (err, res) => {
-      if (err) {
-        console.error(err);
-      }
-      resp.status(201).json(res.rows[0]);
-    },
-  );
+  pool
+    .connect()
+    .then((client) => client
+      .query(
+        'INSERT INTO users (name, email, password, create_time) VALUES ($1, $2, $3, $4) RETURNING id;',
+        [name, email, saltedPassword, createTime],
+      )
+      .then((res) => {
+        client.release();
+        resp.status(201).json(res.rows[0]);
+      }))
+    .catch((err) => {
+      console.error(err);
+    });
 };
 
 const updateUser = (req, resp) => {
@@ -98,27 +116,35 @@ const updateUser = (req, resp) => {
     resp.status(400).send('cannot have empty name or bio');
     return;
   }
-  pool.query(
-    'UPDATE users SET name = $1, bio = $2 WHERE id = $3;',
-    [name, bio, id],
-    (err, res) => {
-      if (err) {
-        console.error(err);
-      }
-      resp.status(200).send(`user ${id} info updated`);
-    },
-  );
+  pool
+    .connect()
+    .then((client) => client
+      .query('UPDATE users SET name = $1, bio = $2 WHERE id = $3;', [
+        name,
+        bio,
+        id,
+      ])
+      .then((res) => {
+        client.release();
+        resp.status(200).send(`user ${id} info updated`);
+      }))
+    .catch((err) => {
+      console.error(err);
+    });
 };
 
 const deleteUser = (req, resp) => {
   const id = parseInt(req.params.id, 10);
 
-  pool.query('DELETE FROM users WHERE id = $1;', [id], (err, res) => {
-    if (err) {
+  pool
+    .connect()
+    .then((client) => client.query('DELETE FROM users WHERE id = $1;', [id]).then((res) => {
+      client.release();
+      resp.status(200).send(`user deleted, id: ${id}`);
+    }))
+    .catch((err) => {
       console.error(err);
-    }
-    resp.status(200).send(`user deleted, id: ${id}`);
-  });
+    });
 };
 
 const usersClient = {
