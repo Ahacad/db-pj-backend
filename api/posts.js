05964e72cadda46e399560ea36d0fdb6b0b98949
ahacad <1393345075@qq.com>
2021-04-04 15:56:07 +0800

    feat(api/posts): rewrite all apis with promises

--- api/posts.js
+++ api/posts.js
@@ -4,15 +4,18 @@ const addPost = (req, resp) => {
   const { userid, title, content } = req.body;
   const createTime = new Date().toISOString();
   pool
-    .query('INSERT INTO contents (content) VALUES ($1) RETURNING id;', [
-      content,
-    ])
-    .then((res) => {
-      pool.query(
-        'INSERT INTO posts (userid, title, create_time, content_id) VALUES ($1, $2, $3, $4)',
-        // TODO: insert into post
-      );
-    })
+    .connect()
+    .then((client) => client
+      .query('INSERT INTO contents (content) VALUES ($1) RETURNING id;', [
+        content,
+      ])
+      .then((res) => {
+        client.release();
+        pool.query(
+          'INSERT INTO posts (userid, title, create_time, content_id) VALUES ($1, $2, $3, $4)',
+          // TODO: insert into post
+        );
+      }))
     .catch((err) => {
       console.error(err);
     });
