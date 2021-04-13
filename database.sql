1fbe4be99bcaf9198a262ae1508926847d7f12fe
ahacad <1393345075@qq.com>
2021-04-13 21:25:02 +0800

    feat(database): add mock for replies

--- database.sql
+++ database.sql
@@ -9,6 +9,8 @@ CREATE TABLE users (
     PRIMARY KEY(id)
 );
 
+-- user_type: 0 admin, 1 user
+
 CREATE TABLE contents (
     id SERIAL UNIQUE, 
     content varchar(10000),
@@ -35,7 +37,7 @@ CREATE TABLE replies (
     id SERIAL UNIQUE,
     userid int,
     post_id int,
-    reply_time timestamp,
+    create_time timestamp,
     likecount int,
     content_id int,
     PRIMARY KEY(id),
@@ -44,18 +46,33 @@ CREATE TABLE replies (
 );
 
 -- mock data
+
+-- users
 INSERT INTO users (name, email, password, create_time, user_type) VALUES ('test1', 'test1@foo.com', 'test1', '2021-04-04T11:19:57.563Z', 1);
 INSERT INTO users (name, email, password, create_time, user_type) VALUES ('test2', 'test2@foo.com', 'test2', '2021-04-04T11:19:57.563Z', 1);
 INSERT INTO users (name, email, password, create_time, user_type) VALUES ('test3', 'test3@foo.com', 'test3', '2021-04-04T11:19:57.563Z', 1);
 INSERT INTO users (name, email, password, create_time, user_type) VALUES ('test4', 'test4@foo.com', 'test4', '2021-04-04T11:19:57.563Z', 1);
 INSERT INTO users (name, email, password, create_time, user_type) VALUES ('test5', 'test5@foo.com', 'test5', '2021-04-04T11:19:57.563Z', 1);
 
+-- contents
 INSERT INTO contents (content) VALUES ('content1');
 INSERT INTO contents (content) VALUES ('content2');
 INSERT INTO contents (content) VALUES ('content3');
 INSERT INTO contents (content) VALUES ('content4');
 
+INSERT INTO contents (content) VALUES ('reply1');
+INSERT INTO contents (content) VALUES ('reply2');
+INSERT INTO contents (content) VALUES ('reply3');
+INSERT INTO contents (content) VALUES ('reply4');
+
+-- posts
 INSERT INTO posts (userid, create_time, content_id, title) VALUES (1, '2021-04-04T11:19:57.563Z', 1, 'title1');
 INSERT INTO posts (userid, create_time, content_id, title) VALUES (1, '2021-04-04T11:19:57.563Z', 2, 'title2');
 INSERT INTO posts (userid, create_time, content_id, title) VALUES (1, '2021-04-04T11:19:57.563Z', 3, 'title3');
 INSERT INTO posts (userid, create_time, content_id, title) VALUES (1, '2021-04-04T11:19:57.563Z', 4, 'title4');
+
+-- replies
+INSERT INTO replies (userid, post_id, create_time, likecount, content_id) VALUES (1, 1, '2021-04-04T11:19:57.563Z', 1, 5);
+INSERT INTO replies (userid, post_id, create_time, likecount, content_id) VALUES (1, 1, '2021-04-04T11:19:57.563Z', 1, 6);
+INSERT INTO replies (userid, post_id, create_time, likecount, content_id) VALUES (1, 2, '2021-04-04T11:19:57.563Z', 1, 7);
+INSERT INTO replies (userid, post_id, create_time, likecount, content_id) VALUES (1, 2, '2021-04-04T11:19:57.563Z', 1, 8);
