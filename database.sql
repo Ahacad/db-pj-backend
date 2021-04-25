57b7aef0e6d71c2ab301549a889bab12ac41d735
ahacad <1393345075@qq.com>
2021-04-25 15:45:27 +0800

    feat(database): add new table department

--- database.sql
+++ database.sql
@@ -17,19 +17,28 @@ CREATE TABLE contents (
     PRIMARY KEY(id)
 );
 
+CREATE TABLE departments ( 
+    id SERIAL UNIQUE,
+    name varchar(200),
+    description varchar(2000),
+    PRIMARY KEY(id)
+);
+
 CREATE TABLE posts (
     id SERIAL UNIQUE,
     userid int NOT NULL,
-    title varchar(200),
+    title varchar(200) NOT NULL,
     create_time timestamp NOT NULL,
     replycount int DEFAULT 0,
     likecount int DEFAULT 0,
     reply_userid int,
     last_reply_time timestamp ,
     content_id int NOT NULL,
+    department_id int NOT NULL,
     PRIMARY KEY(id),
     FOREIGN KEY(userid) REFERENCES users(id),
-    FOREIGN KEY(content_id) REFERENCES contents(id)
+    FOREIGN KEY(content_id) REFERENCES contents(id),
+    FOREIGN KEY(department_id) REFERENCES departments(id)
 );
 
 
@@ -68,11 +77,14 @@ INSERT INTO contents (content) VALUES ('reply2');
 INSERT INTO contents (content) VALUES ('reply3');
 INSERT INTO contents (content) VALUES ('reply4');
 
+-- departments
+INSERT INTO departments (name, description) VALUES ('Programming', 'A forum for programming');
+
 -- posts
-INSERT INTO posts (userid, create_time, content_id, title) VALUES (1, '2021-04-04T11:19:57.563Z', 1, 'What does “>_” mean in run command?');
-INSERT INTO posts (userid, create_time, content_id, title) VALUES (1, '2021-04-04T11:19:57.563Z', 2, 'Sourcing bash file to set environment variables (Linux)');
-INSERT INTO posts (userid, create_time, content_id, title) VALUES (1, '2021-04-04T11:19:57.563Z', 3, 'Help me please with ” Association Rule (Apriori algorithm) ” in Python');
-INSERT INTO posts (userid, create_time, content_id, title) VALUES (1, '2021-04-04T11:19:57.563Z', 4, 'Help regarding Discord Bot cloning (Beginner)');
+INSERT INTO posts (department_id, userid, create_time, content_id, title) VALUES (1, 1, '2021-04-04T11:19:57.563Z', 1, 'What does “>_” mean in run command?');
+INSERT INTO posts (department_id, userid, create_time, content_id, title) VALUES (1, 1, '2021-04-04T11:19:57.563Z', 2, 'Sourcing bash file to set environment variables (Linux)');
+INSERT INTO posts (department_id, userid, create_time, content_id, title) VALUES (1, 1, '2021-04-04T11:19:57.563Z', 3, 'Help me please with ” Association Rule (Apriori algorithm) ” in Python');
+INSERT INTO posts (department_id, userid, create_time, content_id, title) VALUES (1, 1, '2021-04-04T11:19:57.563Z', 4, 'Help regarding Discord Bot cloning (Beginner)');
 
 -- replies
 INSERT INTO replies (userid, post_id, create_time, likecount, content_id) VALUES (1, 1, '2021-04-04T11:19:57.563Z', 1, 5);
