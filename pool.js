aeda6385b4709e85b03eda2501bb7de9941b973b
ahacad <ahacad@126.com>
2021-06-09 22:48:16 +0800

    chore(pool): using mysql pooling now

--- pool.js
+++ pool.js
@@ -1,23 +1,38 @@
 /*
  *postgresql pooling
  */
-const { Pool } = require("pg");
+const mysql = require("mysql");
 
 require("dotenv").config();
 
-const poolwrite = new Pool({
+// const poolwrite = new Pool({
+// user: "ahacad",
+// password: "root",
+// host: process.env.DB_HOST,
+// database: "api",
+// port: 5432,
+//});
+// const poolread = new Pool({
+// user: "ahacad",
+// password: "root",
+// host: process.env.DB_HOST,
+// database: "api",
+// port: 5432,
+//});
+
+const poolwrite = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.DB_HOST,
   user: "ahacad",
   password: "root",
-  host: process.env.DB_HOST,
   database: "api",
-  port: 5432,
 });
-const poolread = new Pool({
+const poolread = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.DB_HOST,
   user: "ahacad",
   password: "root",
-  host: process.env.DB_HOST,
   database: "api",
-  port: 5432,
 });
 
 module.exports = {
